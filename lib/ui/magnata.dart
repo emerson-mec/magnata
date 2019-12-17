a6072f3913e882e28254593832efeb949c017f35
Emerson Oliveira <memensoamo@gmail.com>
2019-12-17 20:30:56 -0300

    dev sinc

--- lib/ui/magnata.dart
+++ lib/ui/magnata.dart
@@ -89,7 +89,7 @@ class _MagnataState extends State<Magnata> {
               ),
             ),
             Expanded(
-              //BOTÃO "MAIS GRANA"
+              //BOTÃO "MAIS GRANA!!!"
               child: Center(
                 child: FlatButton(
                   onPressed: _mandaGrana,
@@ -98,7 +98,7 @@ class _MagnataState extends State<Magnata> {
                   textColor: Colors.white,
                   color: Colors.deepPurple,
                   child: Text(
-                    "Mais grana!!!!",
+                    "Mais grana!!!!+",
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.w300,
